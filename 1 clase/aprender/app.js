0c098a163ab90c381625de6c715b6ce1c8251616
Andres <andresinsuastyd10@gmail.com>
2021-01-13 18:26:22 -0500

    v-if v-show

---
+++ 1 clase/aprender/app.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+
+    data (){
+        return{
+            name: 'Bitcoin',
+            img: 'https://bitcoin.org/img/icons/opengraph.png?1608131429',
+            changePercent: -10,
+        }
+    }
+})
