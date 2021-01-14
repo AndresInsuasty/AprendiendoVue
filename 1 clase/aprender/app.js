9d50d48e1ff089d591755420e60e84655c417c2f
Andres <andresinsuastyd10@gmail.com>
2021-01-14 11:05:53 -0500

    clases dinamicas

--- 1 clase/aprender/app.js
+++ 1 clase/aprender/app.js
@@ -1,21 +1,28 @@
 new Vue({
     el: '#app',
 
-    data (){
-        return{
+    data() {
+        return {
             name: 'Bitcoin',
             img: 'https://bitcoin.org/img/icons/opengraph.png?1608131429',
             changePercent: -10,
-            prices: [1000,2000,3000,4000,5000,6000,7000],
-            pricesWithDays:[
-                {day:'Lunes',value:1000},
-                {day:'Martes',value:2000},
-                {day:'Miercoles',value:3000},
-                {day:'Jueves',value:4000},
-                {day:'Viernes',value:5000},
-                {day:'Sabado',value:6000},
-                {day:'Domingo',value:7000},
-            ]
+            price: 4000,
+            pricesWithDays: [
+                { day: 'Lunes', value: 1000 },
+                { day: 'Martes', value: 2000 },
+                { day: 'Miercoles', value: 3000 },
+                { day: 'Jueves', value: 4000 },
+                { day: 'Viernes', value: 5000 },
+                { day: 'Sabado', value: 6000 },
+                { day: 'Domingo', value: 7000 },
+            ],
+
+            showPrices: false
+        }
+    },
+    methods: {
+        toggleShowPrices() {
+            this.showPrices = !this.showPrices
         }
     }
 })
